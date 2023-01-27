775bb3d0776de1f165d8ade1fa7f3ff6972fd016
Srini <srini25vasu@gmail.com>
2023-01-27 17:36:22 +0100

    added Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Navbar = (): JSX.Element => {
+    return (
+        <div className="bg-green-50 fixed top-0 w-[100%] z-10">
+            <div className="container max-w-5xl mx-auto flex justify-between items-center py-4">
+                <div className="text-[24px]">Srini K</div>
+                <div className="flex gap-6">
+                    <Link href="/" className="hover:text-[red]"> Home</Link>
+                    <Link href="/about" className="hover:text-[red]"> About</Link>
+                    <Link href="/portfolio" className="hover:text-[red]"> Portfolio</Link>
+                    <Link href="/contact" className="hover:text-[red]"> Contact</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Navbar;
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Navbar from '../components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <main>
-        NextJ Srini
-      </main>
-    </>
+    <div>
+      <Head>
+        <title> Srinivasu Kakaraparti | Portfolio </title>
+        <meta name='description'>
+        </meta>
+        <link rel= 'icon' href='/favicon.ico'/>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+    </div>
   )
 }
